469f42d8a319b51baa8b6485c16b1b6172ffa3da
Sophie B. <stb.github@gmail.com>
2025-10-30 22:02:17 +0100

    Chapter 9, Chapter 10

--- app/page.tsx
+++ app/page.tsx
@@ -41,7 +41,7 @@ export default function Page() {
             alt="Screenshots of the dashboard project showing desktop version"
           />
           <Image
-            src='/hero-mobile.png'
+            src="/hero-mobile.png"
             width={560}
             height={620}
             className="md:hidden"
