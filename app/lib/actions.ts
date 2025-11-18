2f5fd150d6ea15f1beae0ff48866d7e8b87ad420
Sophie B. <stb.github@gmail.com>
2025-11-18 13:57:21 +0100

    Chapter 12 - Error handling

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,24 +1,24 @@
-'use server'
+'use server';
 
 import { z } from 'zod';
 import { revalidatePath } from 'next/cache';
 import postgres from 'postgres';
 import { redirect } from 'next/navigation';
 
-const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require'});
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 const InvoiceFormSchema = z.object({
   id: z.string(),
   customerId: z.string(),
   amount: z.coerce.number(),
   status: z.enum(['pending', 'paid']),
-  date: z.string()
-})
+  date: z.string(),
+});
 
 const CreateInvoiceSchema = InvoiceFormSchema.omit({
   id: true,
   date: true,
-})
+});
 
 export async function createInvoiceAction(formData: FormData) {
   const { customerId, amount, status } = CreateInvoiceSchema.parse({
@@ -26,14 +26,23 @@ export async function createInvoiceAction(formData: FormData) {
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
-  
+
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
-  
-  await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-  `
+
+  try {
+    await sql`
+        INSERT INTO invoices (customer_id, amount, status, date)
+        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
+  } catch (error) {
+    console.error(error);
+
+    return {
+      message: 'Database Error: Failed to Create Invoice',
+    };
+  }
+
 
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
@@ -51,17 +60,29 @@ export async function updateInvoiceAction(id: string, formData: FormData) {
 
   const amountInCents = amount * 100;
 
-  await sql`
-    UPDATE invoices
-    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-    WHERE id = ${id}
-  `;
+  try {
+    await sql`
+        UPDATE invoices
+        SET customer_id = ${customerId},
+            amount      = ${amountInCents},
+            status      = ${status}
+        WHERE id = ${id}
+    `;
+  } catch (error) {
+    // We'll also log the error to the console for now
+    console.error(error);
+    return { message: 'Database Error: Failed to Update Invoice.' };
+  }
 
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
 
 export async function deleteInvoiceAction(id: string) {
-  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  throw new Error('Failed to Delete Invoice');
+  
+  await sql`DELETE
+            FROM invoices
+            WHERE id = ${id}`;
   revalidatePath('/dashboard/invoices');
 }
